eaee70821120876453de342218200187577f2b3c
AlaBenAicha <ala.ben.aicha23@gmail.com>
2021-11-07 14:41:43 +0100

    rechanged deployment url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://map-locations-nkxl6gb2n-alabenaicha.vercel.app";
+    : "map-locations-sigma.vercel.app";
 
 export default baseUrl;
